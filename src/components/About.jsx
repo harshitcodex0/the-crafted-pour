1596448133b79966b611dfb294e0c318605d0a97
Harshit <harshitchoudhary624@hotmail.com>
2026-06-14 00:13:09 +0530

    Worked on about Section

---
+++ src/components/About.jsx
@@ -0,0 +1,90 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { SplitText } from "gsap/all"
+
+const About = () => {
+
+    useGSAP(() => {
+        const titlesplit = SplitText.create('#about h2', {
+            type: 'words',
+        })
+
+        const ScrollTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#about',
+                start: 'top center',
+            }
+        })
+
+        ScrollTimeline
+        .from(titlesplit.words, {
+            opacity: 0,
+            duration:1, yPercent: 100, ease: 'expo.out', stagger: 0.02
+        })
+
+        .from('.top-grid div, .bottom-grid div', {
+            opacity: 0,
+            duration: 1,
+            ease: 'power1.inOut',
+            stagger: 0.04,
+        }, '-=0.5') // It will make the animation start before previous animation ends
+
+    })
+  return (
+    <div id="about">
+        <div className="mb-16 md-px-0 px-5">
+            <div className="content">
+                <div className="md:col-span-8">
+                    <p className="badge">Best Cocktails</p>
+                    <h2>Where every detail matters <span className="text-white">-</span>
+                    from muddle to garnish</h2>
+                </div>
+
+                <div className="sub-content">
+                    <p>
+                        Every cocktail we serve is a reflection of our obsession with detail - 
+                        from the first muddle to the final garnish. That care is what turns a simple 
+                        drink int something truly memorable.
+                    </p>
+                    <div>
+                        <p className="md:text-3xl text-xl font-bold">
+                            <span>4.5</span>/5
+                        </p>
+                        <p className="text-sm text-white-100">
+                            More than +12000 customers
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="top-grid">
+                <div className="md:col-span-3">
+                    <div className="noisy" />
+                    <img src="/images/abt1.png" alt="Grid Img 1" />
+                </div>
+                  <div className="md:col-span-6">
+                      <div className="noisy" />
+                      <img src="/images/abt2.png" alt="Grid Img 2" />
+                  </div>
+                  <div className="md:col-span-3">
+                      <div className="noisy" />
+                      <img src="/images/abt5.png" alt="Grid Img 5" />
+                  </div>
+            </div>
+
+            <div className="bottom-grid">
+                  <div className="md:col-span-8">
+                      <div className="noisy" />
+                      <img src="/images/abt3.png" alt="Grid Img 3" />
+                  </div>
+                  <div className="md:col-span-4">
+                      <div className="noisy" />
+                      <img src="/images/abt4.png" alt="Grid Img 4" />
+                  </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
