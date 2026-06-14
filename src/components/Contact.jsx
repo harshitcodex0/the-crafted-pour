b9da9907e59b742f09393fec2cc52cd3cbe7e7b6
Harshit <harshitchoudhary624@hotmail.com>
2026-06-14 23:27:55 +0530

    Worked on Menu and Contact Section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,80 @@
+import { openingHours, socials } from "../../constants"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { SplitText } from "gsap/all"
+
+const Contact = () => {
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#contact h2', {
+            type: 'word',
+        });
+
+        const timeline = gsap.timeline({
+            scrollTrigger:{
+                trigger: '#contact',
+                start: 'top center',
+
+            },
+            ease: "power1.inOut"
+        })
+
+        timeline
+        .from(titleSplit.words, {
+            opacity: 0, yPercent: 100, stagger: 0.02,
+        })
+        .from('#contact h3, #contact p', {
+            opacity: 0, yPercent: 100, stagger: 0.02
+        })
+        .to('#f-right-leaf', {
+            y: '-50', duration: 1, ease: 'power1.inOut'
+        })
+        .to('#f-left-leaf', {
+            y: '-50', duration: 1, ease: 'power1.inOut'
+        })
+    })
+
+  return (
+    <footer id="contact">
+        <img src="/images/footer-right-leaf.png" alt="Right Leaf"  id="f-right-leaf"/>
+        <img src="/images/footer-left-leaf.png" alt="Left Leaf"  id="f-left-leaf"/>
+
+        <div className="content">
+            <h2>Where to Find Us</h2>
+
+            <div>
+                <h3>Visit Our Bar</h3>
+                <p>123, GT Central, #007, Jaipur, RAJ 302017</p>
+            </div>
+
+            <div>
+                <h3>Contact Us</h3>
+                <p>(816) 927-4563</p>
+                  <p>stli2rxj5@mozmail.com</p>
+            </div>
+
+            <div>
+                <h3>Open Every Day</h3>
+                  {openingHours.map((time) => (
+                      <p key={time.day}>
+                          {time.day} : {time.time}
+                      </p>
+                  ))}
+            </div>
+
+            <div>
+                <h3>Socials</h3>
+                <div className="flex-center gap-5">
+                    {socials.map((social) => (
+                        <a key={social.name} href={social.url} target="_blank" rel="noopener noreferrer" aria-label={social.name}>
+                            <img src={social.icon} alt="" />
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </footer>    
+)
+}
+
+export default Contact
